1f5d3d07ccf6b79cb0cdb4118bc9e2d019b479b7
garywaller246 <garywaller246@gmail.com>
2017-06-28 13:22:03 +0300

    var select added

--- public/artist.js
+++ public/artist.js
@@ -15,6 +15,8 @@ $(".submit-button").on('click', function() {
     var mySetting2 = $(".setting-input2").val();
     var mySetting3 = $(".setting-input3").val();
 
+    var selected = $('input[name=optradio]:checked').val();
+
     var newObj = {
         name: name,
         bio: bio,
@@ -22,6 +24,7 @@ $(".submit-button").on('click', function() {
         profile_pic: profilePic,
         chains: [myNecklace1, myNecklace2, myNecklace3],
         settings: [mySetting1, mySetting2, mySetting3],
+        theme: selected
     }
 
     $.ajax({
@@ -33,4 +36,14 @@ $(".submit-button").on('click', function() {
             console.log(textStatus);
         }
     });
+    $(".name").val('');
+    $(".bio").val('');
+    $(".email").val('');
+    $(".profilePic").val('');
+    $(".necklace-input1").val('');
+    $(".necklace-input2").val('');
+    $(".necklace-input3").val('');
+    $(".setting-input1").val('');
+    $(".setting-input2").val('');
+    $(".setting-input3").val('');
 });
