0f23f48d113edd662e97b1df1cc9c61a7097ad01
mosh7890 <mosh7890@gmail.com>
2017-06-28 14:26:32 +0300

    edit artists 99% done

--- public/artistEdit.js
+++ public/artistEdit.js
@@ -1,19 +1,31 @@
-var editArtist = function () {
+// Handles Errors
+var errorHandler = function (err, status) {
+    console.error(status);
+}
 
+var editArtist = function (email, option, input) {
+    console.log('/artist/' + email + '/' + option);
+    $.ajax({
+        type: "POST",
+        url: '/artist/' + email + '/option/' + option,
+        data: input,
+        success: function (data) { },
+        error: errorHandler
+    });
 }
 
 
 $('.radio-div1').on('change', '.radio-inline', function () {
     var selected = $('input[name=optradio]:checked').val();
     selected = '.' + selected + 'Div';
-    console.log(selected);
     $(selected).toggle();
     $(selected).siblings().hide();
 });
 
 $('.submit-button').on('click', function () {
     var email = $('.edit-artist-input').val();
-    var input = $(this).siblings('input').val();
-    var edit = $('input[name=optradio]:checked').val();
-    console.log(email + input + edit);
+    var option = $('input[name=optradio]:checked').val();
+    var input = { text: $(this).siblings('input').val() };
+
+    editArtist(email, option, input);
 });
