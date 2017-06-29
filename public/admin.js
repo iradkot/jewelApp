cb07ebdb80bbfaa554b2f9972a86ba9db33e83d9
mosh7890 <mosh7890@gmail.com>
2017-06-29 15:27:36 +0300

    finish

--- public/admin.js
+++ public/admin.js
@@ -3,6 +3,32 @@ var errorHandler = function (err, status) {
     console.error(status);
 }
 
+$.get('admin.hbs', function (source) {
+    template = Handlebars.compile(source);
+    render();
+}, 'html');
+
+var artistList = [];
+
+// Get Artist Data
+var fetch = function () {
+    $.ajax({
+        method: "GET",
+        url: '/artists',
+        success: function (data) {
+            artistList = data;
+            render();
+        },
+        error: errorHandler
+    });
+}
+
+var render = function () {
+    $('.artist-list').empty();
+    var newHTML = template(artistList);
+    $('.artist-list').append(newHTML);
+}
+
 var deleteArtist = function (artistEmail) {
     $.ajax({
         type: "DELETE",
@@ -14,6 +40,7 @@ var deleteArtist = function (artistEmail) {
     })
 }
 
+fetch(); 
 
 $('.remove-artist-button').on('click', function () {
     var artistEmail = $('.remove-artist-input').val();
